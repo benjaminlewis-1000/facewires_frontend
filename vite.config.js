11a4388825a977dd63bf79d2c4f158801993bda8
benjamin.lewis <benjamin.lewis.13@us.af.mil>
2026-08-19 12:52:21 -0400

    Updating for dev work

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    // This ensures Vite runs on the port your Docker setup expects
     port: 3000,
-    // host: true is required for Docker containers to expose the port to the host machine
     host: true, 
+    // This replaces CHOKIDAR_USEPOLLING for Vite
+    watch: {
+      usePolling: true,
+    }
   },
 });
